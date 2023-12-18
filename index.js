bc8d66f5e85fe59653a0372ae8bd64cd99448b3a
EmiDip <di.palma.emilie@outlook.fr>
2023-12-18 11:52:31 +0100

    modif

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
-const name = process.env.MY_NAME || "Default Name";
-const city = process.env.MY_CITY || "Default City";
-const language = process.env.MY_LANGUAGE || "Default Language";
+const name = process.env.MY_NAME || "Bob";
+const city = process.env.MY_CITY || "Aix";
+const language = process.env.MY_LANGUAGE || "Javascript";
 
 console.log(`I am ${name}, wilder in ${city}, and I love ${language}.`);
